c2de3b252b2cca678f550587bc60e0a98fe6a950
Satyaban Sahoo <38886899+hemraj4545@users.noreply.github.com>
2020-11-18 01:51:25 +0530

    Update scikit_models.jl

--- scikit_models.jl
+++ scikit_models.jl
@@ -4,6 +4,8 @@ using ScikitLearn, MLDatasets
 @sk_import metrics: accuracy_score
 @sk_import tree:DecisionTreeClassifier
 @sk_import ensemble: RandomForestClassifier
+@sk_import svm:SVC
+@sk_import naive_bayes: (GaussianNB, BernoulliNB, CategoricalNB, ComplementNB, MultinomialNB)
 
 # Loading the Iris Dataset from MLDatasets.jl
 features = Iris.features()
@@ -21,22 +23,73 @@ fit!(LR, x_train, y_train)
 prediction = predict(LR, x_test)
 
 accuracy = accuracy_score(prediction, y_test)
-print("Accuracy for Logistic Regression: ",string(round(accuracy*100),3))
+println("Accuracy for Logistic Regression : ",string(round(accuracy*100),3))
 
 # Decision Tree Classifier
 DTC = DecisionTreeClassifier()
 
-fit!(DTC, features, labels)
-prediction = predict(DTC, features)
+fit!(DTC, x_train, y_train)
+prediction = predict(DTC, x_test)
 
-accuracy = accuracy_score(prediction, labels)
-print("Accuracy for Decision Tree Classifier: ",string(round(accuracy*100),3))
+accuracy = accuracy_score(prediction, y_test)
+println("Accuracy for Decision Tree Classifier : ",string(round(accuracy*100),3))
 
 # Random Forest Classifier
 RFC = RandomForestClassifier()
 
-fit!(RFC, features, labels)
-prediction = predict(RFC, features)
+fit!(RFC, x_train, y_train)
+prediction = predict(RFC, x_test)
+
+accuracy = accuracy_score(prediction, y_test)
+println("Accuracy for Random Forest Classifier: ",string(round(accuracy*100),3))
+
+# Support Vector Machine
+SVM = SVC()
+
+fit!(SVM, features, labels)
+prediction = predict(SVM, features)
 
 accuracy = accuracy_score(prediction, labels)
-print("Accuracy for Random Forest Classifier: ",string(round(accuracy*100),3))
+println("Accuracy for Support Vector Machine: ",string(round(accuracy*100),3))
+
+# Naive-Bayes Models
+GNB = GaussianNB()
+BNB = BernoulliNB()
+CNB = CategoricalNB() 
+CcNB = ComplementNB()
+MNB = MultinomialNB()
+
+# Gaussian Naive Bayes
+fit!(GNB, features, labels)
+prediction = predict(GNB, features)
+
+accuracy = accuracy_score(prediction, labels)
+println("Accuracy for Gaussian Naive Bayes: ",string(round(accuracy*100),3))
+
+# Bernoulli Naive Bayes
+fit!(BNB, features, labels)
+prediction = predict(BNB, features)
+
+accuracy = accuracy_score(prediction, labels)
+println("Accuracy for Bernoulli Naive Bayes: ",string(round(accuracy*100),3))
+
+# Categorical Naive Bayes
+fit!(CNB, features, labels)
+prediction = predict(CNB, features)
+
+accuracy = accuracy_score(prediction, labels)
+println("Accuracy for Categorical Naive Bayes: ",string(round(accuracy*100),3))
+
+# Complement Naive Bayes
+fit!(CcNB, features, labels)
+prediction = predict(CcNB, features)
+
+accuracy = accuracy_score(prediction, labels)
+println("Accuracy for Complement Naive Bayes: ",string(round(accuracy*100),3))
+
+# Multinomial Naive Bayes
+fit!(MNB, features, labels)
+prediction = predict(MNB, features)
+
+accuracy = accuracy_score(prediction, labels)
+println("Accuracy for Multinomial Naive Bayes: ",string(round(accuracy*100),3))
